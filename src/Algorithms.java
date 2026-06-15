41651e375d474fe762476dd0ff3ae81ea7eb60d9
clifford kpeli DZIDZORM <cdkpeli@st.ug.edu.gh>
2026-06-15 21:08:20 +0000

    Refactor linear and binary search methods
    Fix syntax errors and improve linear and binary search implementations.

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,28 +1,29 @@
 public class Algorithm {
-    //linear search
-    public static linearSearch(int[] array, int target){
-        for (int = 0; 1 < array.length; i++){
-            if (array[i] == target){
+
+    public static int linearSearch(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
                 return i;
             }
         }
         return -1;
     }
-}
-       //binary search
-public static int binarySearch(int[] array, int target){
-    int left = 0
-            int right = array.length - 1;
-    while (left <= right ){
-        int mid = left + (right - left) / 2;
-        if (array[mid] == target){
-            return mid;
-          else if (array[mid] < target) {
-              left = mid + 1;
-              else
-                  right = mid - 1;
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
+        return -1;
     }
-    return -1;
 }
